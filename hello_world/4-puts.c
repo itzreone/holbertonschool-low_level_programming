9afa61dd3a4fa982a1580a0b0d7370921a65fdec
root <ssreone@gmail.com>
2024-02-21 05:30:28 -0800

    upd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-* main - puts 
+* main - puts
 * Return : 0
 */
-int main(void){
+int main (void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
