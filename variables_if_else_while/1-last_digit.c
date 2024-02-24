8ba91beba971fdf0c73647ee25ccea212fc01d7c
root <ssreone@gmail.com>
2024-02-24 22:28:04 +0000

    upd

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int n, n_digit;
