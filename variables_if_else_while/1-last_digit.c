a69b44737f568d25449bded0a794e94aa59c094f
root <ssreone@gmail.com>
2024-02-24 22:26:57 +0000

    upd

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
     {
         printf("Last digit of %d is %d and is 0\n", n, n_digit);
     } 
-    else // Correct indentation here
+    else
     {
         printf("Last digit of %d is %d and is greater than 5\n", n, n_digit); 
     }
