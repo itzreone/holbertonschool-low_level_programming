839308022ae125e50bd7526871874d9f3c2b07de
root <ssreone@gmail.com>
2024-02-24 21:51:45 +0000

    upd

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,4 +19,13 @@ int main(void)
 	if (n_digit < 6 && last_digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n" n, n_digit);
-
+	}
+	else if (n_digit == 0)
+		printf("Last digit of %d is %d and is 0\n" n, n_digit);
+	{
+	else
+	{
+		printf("Last digit of %d is %d and is greater than 5\n" n, n_digit);
+	}
+	return (0);
+}
