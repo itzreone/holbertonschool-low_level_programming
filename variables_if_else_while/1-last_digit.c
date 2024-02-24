6917bfa090af437e659eae5d1df6a737e275c395
root <ssreone@gmail.com>
2024-02-24 22:39:46 +0000

    upd

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,18 +16,19 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 n_digit = n % 10;
 
-if (n_digit < 6 && n_digit != 0) 
+if (n_digit < 6 && n_digit != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_digit);
 }
 else if (n_digit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, n_digit);
-} 
+}
 else
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, n_digit); 
+printf("Last digit of %d is %d and is greater than 5\n", n, n_digit);
 }
 
 return (0);
 }
+
