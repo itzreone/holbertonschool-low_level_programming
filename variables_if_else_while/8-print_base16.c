e7a37b76a132d90b15f6e67d2c89e42cc3c36941
root <ssreone@gmail.com>
2024-02-24 23:50:10 +0000

    upd

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,16 +7,19 @@
  * Description: 'get number'
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 int c;
 
 
-for (c = '0'; c <= '9'; c++) {
+for (c = '0'; c <= '9'; c++)
+{
 putchar(c); 
 }
 
 
-for (c = 'a'; c <= 'f'; c++) {
+for (c = 'a'; c <= 'f'; c++)
+{
 putchar(c);
 }
 
