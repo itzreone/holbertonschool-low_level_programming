f71b95950d6c72d2fc93cb34a8cc830d4f1164f7
root <ssreone@gmail.com>
2024-02-24 23:48:43 +0000

    upd

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,25 +7,20 @@
  * Description: 'get number'
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-        char c;
-        c = 0;
-        while(c <= 10)
-        if(c < 10)
-        {
-            putchar(c);
-            c++;
-        }
-        else if(c = 10)
-        {
-                c = 'a';
-                while(c <= 'f')
-                {
-                        putchar(c);
-                        c++;
-                }
-        }
-        
-        return (0);
+int main(void) {
+int c;
+
+
+for (c = '0'; c <= '9'; c++) {
+putchar(c); 
 }
+
+
+for (c = 'a'; c <= 'f'; c++) {
+putchar(c);
+}
+
+putchar('\n');
+return 0;
+}
+
