c2c2dab455c3e64a3d9715a808da01967b51f21f
root <ssreone@gmail.com>
2024-02-24 11:35:11 +0000

    upd

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,15 +8,15 @@
  * Description: 'get number'
  * Return: Always 0 (Success)
  */
-int main() 
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
