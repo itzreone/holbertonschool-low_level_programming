2b7e033540163d19565d3e31afca5da85f1662b8
root <ssreone@gmail.com>
2024-02-25 11:05:09 +0000

    upd

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+}
+
