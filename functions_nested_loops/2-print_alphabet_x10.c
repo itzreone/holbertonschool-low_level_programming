536416c72e2648ba94540fa1259b220308d38245
root <ssreone@gmail.com>
2024-02-25 13:31:40 +0000

    upd

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,23 @@
 #include "main.h"
-
 /**
- * print_alphabet - prints the alphabet
+ * print_alphabet_x10 - prints the alphabet 10 times
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-        char c;
-	int i;
-	i = 0;
+char c;
+int i;
+i = 0;
 
-	while(i < 10)
-	{
-        	for (c = 'a' ; c <= 'z' ; c++)
-        	{
-                	_putchar(c);
-        	}
-        	_putchar('\n');
-		i++;
-	}
+while(i < 10)
+{
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
 }
- 
+_putchar('\n');
+i++;
+}
+}
+
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void int _islower(int c);
+{
+	
+}
