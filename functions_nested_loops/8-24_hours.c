443fb55924eac82e5074e2a8c74debda28009b53
root <ssreone@gmail.com>
2024-02-25 14:59:19 +0000

    upd

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,37 +9,37 @@ void jack_bauer(void)
 {
 	char n1, n2, n3, n4;
 
-	for (n1 = '0', n1 <= '2', n1++);
+	for (n1 = '0'; n1 <= '2'; n1++)
 	{
 		if (n1 == '0' || n1 == '1')
-			for (n2 = '0', n <=9, n2++);
+			for (n2 = '0', n2 <=9, n2++);
 			{
 				for (n3 = '0', n3 <= '5', n3++);
 				{
 					for (n4 = '0', n4 <= '9', n4++);
 					{
-					_putchar(c1);
-					_putchar(c2);
-					_putchar(':');
-					_putchar(c3);
-					_putchar(c4);
-					_putchar('\n');
+						_putchar(c1);
+						_putchar(c2);
+						_putchar(':');
+						_putchar(c3);
+						_putchar(c4);
+						_putchar('\n');
 					}
 				}
 			}
 		else if (n1 == '2')
-			for (n2 = '0', n <=3, n2++);
+			for (n2 = '0', n2 <=3, n2++);
 			{
 				for (n3 = '0', n3 <= '5', n3++);
 				{
 					for (n4 = '0', n4 <= '9', n4++);
 					{
-					_putchar(c1);
-					_putchar(c2);
-					_putchar(':');
-					_putchar(c3);
-					_putchar(c4);
-					_putchar('\n');
+						_putchar(c1);
+						_putchar(c2);
+						_putchar(':');
+						_putchar(c3);
+						_putchar(c4);
+						_putchar('\n');
 					}
 				}
 			}
