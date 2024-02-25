2a259a4a90876815c02b69eb7438347511bb1b61
root <ssreone@gmail.com>
2024-02-25 15:02:21 +0000

    upd

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,15 +14,15 @@ void jack_bauer(void)
 		if (n1 == '0' || n1 == '1')
 			for (n2 = '0'; n2 <=9; n2++)
 			{
-				for (n3 = '0'; n3 <= '5'; n3++;)
+				for (n3 = '0'; n3 <= '5'; n3++)
 				{
 					for (n4 = '0'; n4 <= '9'; n4++)
 					{
-						_putchar(c1);
-						_putchar(c2);
+						_putchar(n1);
+						_putchar(n2);
 						_putchar(':');
-						_putchar(c3);
-						_putchar(c4);
+						_putchar(n3);
+						_putchar(n4);
 						_putchar('\n');
 					}
 				}
@@ -34,11 +34,11 @@ void jack_bauer(void)
 				{
 					for (n4 = '0'; n4 <= '9'; n4++);
 					{
-						_putchar(c1);
-						_putchar(c2);
+						_putchar(n1);
+						_putchar(n2);
 						_putchar(':');
-						_putchar(c3);
-						_putchar(c4);
+						_putchar(n3);
+						_putchar(n4);
 						_putchar('\n');
 					}
 				}
