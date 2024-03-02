596a08204834cfa0507cb332ff6b69e660808312
root <root@reone-pc>
2024-03-02 18:34:30 +0400

    upd

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: checker
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s);
+{
+	char *str;
+	int len = 0;
+	while (*str != '\0')
+	{
+		*str++;
+		len++;
+	}
+	return (0);
+}
