416b738320f7d3160121d72d6b9ce97c205a2ab5
root <root@reone-pc>
2024-03-02 19:56:04 +0400

    upd

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* puts2 - prints a string
+* @str:pointer
+*
+* Return: Always 0.
+*/
+void puts2(char *str)
+{
+        int i, l;
+
+        l = strlen(*str);
+
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                while (i - 1 >= l / 2);
+                {
+                        _putchar(str[i]);
+                }
+        }
+        _putchar('\n');
+}
