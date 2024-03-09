12c32811fb33d60fc6a2d16fdfea34eb8a69bb0d
root <root@reone-pc>
2024-03-10 01:15:29 +0400

    upd

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - check the code
+ * @a: pointer
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+for (int i = 0; i < 8; i++) {
+        for (int p = 0; p < 8; p++) {
+            putchar(a[i][p]);
+        }
+        putchar('\n'); // Add a newline after each row
+    }
+}
