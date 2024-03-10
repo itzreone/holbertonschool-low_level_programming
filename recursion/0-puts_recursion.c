b29fce744a049fcd7dd112868bae3d513a3d98d6
root <root@reone-pc>
2024-03-10 20:00:12 +0400

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,9 +1,19 @@
-#include<stdio.h>
+#include "main.h"
 /**
-*_puts_recursion - function print a string
-*@s: variable pointer
-*/
+ * _puts_recursion - fills memory
+ * @s: data
+ *
+ * Return: char
+ */
 void _puts_recursion(char *s)
 {
-	printf("%s\n", s);
+	if (*s == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include "main.h"
 /**
-*_print_rev_recursion - function print a string in reverse
-*@s: variable pointer
-*/
+ * _print_rev_recursion - fills memory
+ * @s: data
+ *
+ * Return: char
+ */
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i])
+	if (*s != 0)
 	{
 		_print_rev_recursion(s + 1);
-		putchar(s[i]);
+		_putchar(*s);
 	}
 }
