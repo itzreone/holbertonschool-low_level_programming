eb88af98ae42e17231c85076b6b1c00888312fa3
root <root@reone-pc>
2024-03-17 19:32:33 +0400

    upd

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,48 +1,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "main.h"
-/**
-*str_concat - function concat two pointers
-*@s1: variable pointer char
-*@s2: variable pointer char
-*Return: pointer char
-*/
+
 char *str_concat(char *s1, char *s2)
 {
-	int len = 0;
-	int len2 = 0;
-	int i, j;
-	char *cat = NULL;
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	len = strlen(s1);
-	len2 = strlen(s2);
-	cat = malloc(sizeof(char) * (len + len2) + 1);
-	if (cat == NULL)
-	{
+	char *s;
+	int size1 = 0, size2 = 0, i, k = 0, j = 0;
+    
+	if (s1 != NULL)
+		while (s1[i] != '\0')
+        {	    
+		    size1++;
+		    i++;
+        }
+	if (s2 != NULL)
+		while (s2[i] != '\0')
+        {	
+		    size2++;
+		    j++;
+        }
+	s = malloc(size1 + size2 + 1);
+	if (s == NULL)
 		return (NULL);
-	}
-	i = 0;
-	while (i < len)
-	{
-		cat[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j < len2)
-	{
-		cat[i] = s2[j];
-		i++;
-		j++;
-	}
-	cat[i] = '\0';
-	return (cat);
+	for (i = 0 ; i < size1 ; i++)
+		s[i] = s1[i];
+	for (i = 0 ; i < size2 ; i++)
+		s[size1 + i] = s2[i];
+	s[size1 + size2] = '\0';
+
+	return (s);
 }
